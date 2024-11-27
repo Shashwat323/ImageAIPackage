0d935aafd2d9ee040e035382988f06894c3445b8
sansworthy <alijan.osman123@outlook.com>
2024-11-27 19:10:31 +1000

    all functions added

--- imageaipackagetests.py
+++ imageaipackagetests.py
@@ -24,6 +24,37 @@ class ImageAIPackageTests(unittest.TestCase):
         new_img = iap.region_grow(iap.image_path_to_np_array("unit_test_images/TestImg2.jpg"), 10)
         iap.save_image(new_img, "unit_test_images/RegionGrowImg1.jpg")
 
+    def test_overlay_images(self):
+        base_img = iap.image_path_to_np_array("unit_test_images/TestImg1.jpg")
+        overlay_img = iap.image_path_to_np_array("unit_test_images/TestImg2.jpg")
+        new_img = iap.overlay_images(base_img, overlay_img, transparency=0.5)
+        iap.save_image(new_img, "unit_test_images/overlay_images_test.jpg")
+
+    def test_adjust_contrast(self):
+        new_img = iap.adjust_contrast(iap.image_path_to_np_array("unit_test_images/TestImg1.jpg"), factor=1.5, chance=1.0)
+        iap.save_image(new_img, "unit_test_images/adjust_contrast_test.jpg")
+
+    def test_adjust_hue(self):
+        new_img = iap.adjust_hue(iap.image_path_to_np_array("unit_test_images/TestImg1.jpg"), factor=1.2, chance=1.0)
+        iap.save_image(new_img, "unit_test_images/adjust_hue_test.jpg")
+
+    def test_adjust_brightness(self):
+        new_img = iap.adjust_brightness(iap.image_path_to_np_array("unit_test_images/TestImg1.jpg"), factor=1.2, chance=1.0)
+        iap.save_image(new_img, "unit_test_images/adjust_brightness_test.jpg")
+
+    def test_square_rotate(self):
+        new_img = iap.square_rotate(iap.image_path_to_np_array("unit_test_images/TestImg1.jpg"), mode=2, chance=1.0)
+        iap.save_image(new_img, "unit_test_images/square_rotate_test.jpg")
+
+    def test_mirror_image(self):
+        new_img = iap.mirror_image(iap.image_path_to_np_array("unit_test_images/TestImg1.jpg"), chance=1.0)
+        iap.save_image(new_img, "unit_test_images/mirror_image_test.jpg")
+
+    def test_random_rotate(self):
+        new_img = iap.random_rotate(iap.image_path_to_np_array("unit_test_images/TestImg1.jpg"))
+        iap.save_image(new_img, "unit_test_images/random_rotate_test.jpg")
+
+
 
 if __name__ == '__main__':
     unittest.main()
