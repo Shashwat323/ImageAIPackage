634c6a86880a6f365f4b662568e35fe1b22d15db
jfernando02 <jfernando020202@gmail.com>
2024-12-01 15:01:06 +0800

    Fix loader, models, run and training.ipynb. Create pipeline for configurable calls to augmentation. Modify augmentation function parameters. Add flexiblemodel.py to use as basis.

--- loader.py
+++ loader.py
@@ -1,53 +1,79 @@
 import os
+from functools import partial
 
-import pandas as pd
+import imageaipackage as iap
+
 from PIL import Image
 from matplotlib import pyplot as plt
 
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-from torchvision.transforms import ToTensor
+
+resize_to = partial(iap.resize, new_width=64)
+
+augment = iap.TransformPipeline([
+    iap.img_to_numpy,
+    iap.mirror_image,
+    iap.adjust_brightness,
+    iap.adjust_contrast,
+    iap.adjust_hue,
+    iap.square_rotate
+])
+
+normalize = iap.TransformPipeline([
+    iap.img_to_numpy,
+    iap.crop,
+    resize_to
+])
 
 # the original dataset
 class ImageDataset(Dataset):
-    def __init__(self, csv_path, image_folder, y_col_name, transform=None):
-        self.csv = pd.read_csv(csv_path)
-        self.image_folder = image_folder
-
-        # Drop the rows where the image does not exist
-        images = os.listdir(image_folder)
-        self.csv = self.csv[self.csv['name'].isin(images)]
-        self.csv.reset_index(drop=True, inplace=True)
-
-        self.y_col_name = y_col_name
+    def __init__(self, root_dir, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the images organized in subfolders per class.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.image_paths = []
+        self.labels = []
+        self.class_to_idx = {}
         self.transform = transform
 
+        # Traverse the directory to collect image paths and labels
+        for idx, class_folder in enumerate(os.listdir(root_dir)):
+            class_path = os.path.join(root_dir, class_folder)
+            if os.path.isdir(class_path):
+                self.class_to_idx[class_folder] = idx
+                for image_name in os.listdir(class_path):
+                    image_path = os.path.join(class_path, image_name)
+                    if os.path.isfile(image_path):
+                        self.image_paths.append(image_path)
+                        self.labels.append(idx)
+
     def __len__(self):
-        return len(self.csv)
+        return len(self.image_paths)
 
     def __getitem__(self, idx):
-        image_path = os.path.join(self.image_folder, self.csv.iloc[idx, 4])
+        image_path = self.image_paths[idx]
         image = Image.open(image_path)
 
-        # check the channel number
+        # Ensure image is RGB
         if image.mode != 'RGB':
             image = image.convert('RGB')
 
-        y = self.csv.loc[idx, self.y_col_name]
+        label = os.path.basename(os.path.dirname(image_path))
 
         if self.transform:
             image = self.transform(image)
 
-        return image, y
-
-
+        return image, label
 
 # the augmented dataset
 class AugmentedImageDataset(Dataset):
-    def __init__(self, original_dataset, transforms=None):
+    def __init__(self, original_dataset, transform = None):
         self.original_dataset = original_dataset
-        self.transforms = transforms
+        self.transform = transform
 
     def __len__(self):
         return 5 * len(self.original_dataset)
@@ -55,27 +81,8 @@ class AugmentedImageDataset(Dataset):
     def __getitem__(self, idx):
         image, y = self.original_dataset[idx // 5]
 
-        if self.transforms and (idx % 5 != 0):
-            image = self.transforms(image)
-
-        return image, y
-
-
-
-# the dataset transformed (by default for vit inputs)
-class TransformedDataset(Dataset):
-    def __init__(self, original_dataset, transforms=None):
-        self.original_dataset = original_dataset
-        self.transforms = transforms
-
-    def __len__(self):
-        return len(self.original_dataset)
-
-    def __getitem__(self, idx):
-        image, y = self.original_dataset[idx]
-
-        if self.transforms:
-            image = self.transforms(image)
+        if idx % 5 != 0:
+            image = self.transform(image)
 
         return image, y
 
@@ -85,38 +92,29 @@ def show_sample_image(dataset):
     fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(15, 3))
     for i, ax in enumerate(axs):
         image, label = dataset.__getitem__(i)
-        ax.imshow(image.detach().cpu().permute(1, 2, 0), cmap='gray')
-        ax.set_title('gt BMI: ' + str(label))
+        ax.imshow(image, cmap='gray')
+        ax.set_title(str(label))
         ax.axis('off')  # Hide axes
     plt.show()
 
 # split dataset and (optionally) augment and/ or transform it for vit
-def train_val_test_split(dataset, augmented=True, vit_transformed=True, detection=None):
+def train_val_split(dataset):
     val_size = int(0.1 * len(dataset))
-    test_size = int(0.2 * len(dataset))
-    train_size = len(dataset) - val_size - test_size
+    train_size = len(dataset) - val_size
 
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-    if augmented:
-        train_dataset = AugmentedImageDataset(train_dataset)
-
-    if vit_transformed:
-        train_dataset = TransformedDataset(train_dataset)
-        val_dataset = TransformedDataset(val_dataset)
-        test_dataset = TransformedDataset(test_dataset)
-
-    return train_dataset, val_dataset, test_dataset
+    return train_dataset, val_dataset
 
 
 
 # get dataloaders
-def get_dataloaders(batch_size=16, augmented=True, vit_transformed=True, show_sample=False, detection=None):
-    image_dataset = ImageDataset('../data/data.csv', '../data/Images', 'bmi', ToTensor())
-    if show_sample:
-        train_dataset, val_dataset, test_dataset = train_val_test_split(image_dataset, augmented, vit_transformed, detection)
-        show_sample_image(train_dataset)
-    train_dataset, val_dataset, test_dataset = train_val_test_split(image_dataset, augmented, vit_transformed, detection)
+def get_dataloaders(batch_size=16):
+    train_val_dataset = ImageDataset('../dataset/train', normalize)
+    train_val_dataset = AugmentedImageDataset(train_val_dataset, augment)
+    test_dataset = ImageDataset('../dataset/test', normalize)
+
+    train_dataset, val_dataset = train_val_split(train_val_dataset)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size,  shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
@@ -126,4 +124,6 @@ def get_dataloaders(batch_size=16, augmented=True, vit_transformed=True, show_sa
 
 # for test
 if __name__ == "__main__":
-    get_dataloaders(augmented=False, vit_transformed=False, show_sample=True, detection="none")
+    dataset = ImageDataset("D:\\Other\\Repos\\ImageAIPackage\\dataset\\train", transform=normalize)
+    augmented_dataset = AugmentedImageDataset(dataset, transform=augment)
+    show_sample_image(augmented_dataset)
