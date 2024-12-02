52a9447ebbcee3c81645eeef6dfc560d27db516a
Cohen Farmer <89297346+Jackforst736@users.noreply.github.com>
2024-12-02 21:36:42 +1000

    Fixed an Error

--- classification.py
+++ classification.py
@@ -13,18 +13,18 @@ class block(nn.Module):
     def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
         super(block, self).__init__()
         self.expansion = 4
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
         self.relu = nn.ReLU()
         self.identity_downsample = identity_downsample
-        self.out_channels = out_channels
+        self.stride = stride
 
     def forward(self, x):
-        identity = x
+        identity = x.clone()
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -85,7 +85,7 @@ class resnet(nn.Module):
 
         if stride != 1 or self.in_channels != out_channels * 4:
             identity_downsample = nn.Sequential(nn.Conv2d(self.in_channels, out_channels*4,
-                                                          kernel_size=1, stride=stride, padding=0),
+                                                          kernel_size=1, stride=stride, bias=False),
                                                 nn.BatchNorm2d(out_channels*4))
         layers.append(block(self.in_channels, out_channels, identity_downsample, stride))
         self.in_channels = out_channels*4
