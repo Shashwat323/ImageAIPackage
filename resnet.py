6193c9fdb902716c3a70fd12b2bc7e7a72cc915c
Cohen Farmer <89297346+Jackforst736@users.noreply.github.com>
2024-12-07 17:11:52 +1000

    Modify something

--- resnet.py
+++ resnet.py
@@ -35,8 +35,8 @@ class resnet(nn.Module):
         super(resnet, self).__init__()
         self.in_channels = 64
 
-        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        #self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        #self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
