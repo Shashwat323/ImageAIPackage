46fec20251ef699b7c9b3776f21468bb58467159
Cohen Farmer <89297346+Jackforst736@users.noreply.github.com>
2024-12-15 20:01:39 +1000

    Add imageseg.py for instance segmentation using YOLO, also added images to test with

---
+++ imageseg.py
@@ -0,0 +1,19 @@
+from IPython import display
+import os
+import ultralytics
+import cv2
+from ultralytics import YOLO
+
+HOME = os.getcwd()
+display.clear_output()
+ultralytics.checks()
+
+model = YOLO(f'{HOME}/weights/yolov8n-seg.pt')
+
+#results = model.train(data='C:/Users/cohen/OneDrive/Documents/GitHub/ImageAIPackage/datasets/jellyfishdataset/data.yaml', epochs=50, imgsz=640)
+
+img = cv2.imread("unit_test_images/CATANDDOG.jpg")
+
+results = model.predict(img)
+for result in results:
+    result.show()
