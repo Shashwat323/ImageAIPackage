d5e130d4f15bc9fc7d7ecd3dc8823c70cc6681e5
Cohen Farmer <89297346+Jackforst736@users.noreply.github.com>
2024-12-15 20:24:07 +1000

    Changed from show

--- imageseg.py
+++ imageseg.py
@@ -8,10 +8,10 @@ ultralytics.checks()
 
 model = YOLO(f'{HOME}/weights/yolov8n-seg.pt')
 
-#results = model.train(data='C:/Users/cohen/OneDrive/Documents/GitHub/ImageAIPackage/datasets/jellyfishdataset/data.yaml', epochs=50, imgsz=640)
+results = model.train(data='C:/Users/cohen/OneDrive/Documents/GitHub/ImageAIPackage/datasets/jellyfishdataset/data.yaml', epochs=50, imgsz=640)
 
 img = cv2.imread("unit_test_images/CATANDDOG.jpg")
 
 results = model.predict(img)
 for result in results:
-    result.show()
+    result.save(filename="result.jpg")
