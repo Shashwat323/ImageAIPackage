97e7a54f1eb5dfd4f0c3f26094581d8a296339fc
Cohen Farmer <89297346+Jackforst736@users.noreply.github.com>
2024-12-16 22:07:04 +1000

    Accidentally did stuff in main

--- imageseg.py
+++ imageseg.py
@@ -6,12 +6,16 @@ from ultralytics import YOLO
 HOME = os.getcwd()
 ultralytics.checks()
 
-model = YOLO(f'{HOME}/weights/yolov8x-seg.pt')
+def createModel(path):
+    return YOLO(path)
 
+def trainModel(dataPath, epochs, imgSize):
+    return model.train(data=dataPath, epochs=epochs, imgsz=imgSize)
+
+
+model = createModel(f'{HOME}/weights/yolov8x-seg.pt')
 results = model.train(data='C:/Users/cohen/OneDrive/Documents/GitHub/ImageAIPackage/datasets/turtle/data.yaml', epochs=10, imgsz=320)
 
 img = cv2.imread("unit_test_images/ANIMALS3.jpg")
 
 results = model.predict(img)
-#for result in results:
-    #result.show()
